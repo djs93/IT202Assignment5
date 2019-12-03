ee2fad1531e5eb6e7977e31c6d8d4b7d0ab43efd
Dale Schofield <djs93@njit.edu>
2019-12-02 20:45:54 -0500

    Initial barebones commit

---
+++ ChatPage.php
@@ -0,0 +1,16 @@
+<?php
+    include("config.php");
+    session_start();
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IT202 Assignment 5</title>
+    <script src="script.js"></script>
+    <style>
+
+    </style>
+</head>
+<body>
+</body>
+
---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_SERVER', "sql1.njit.edu");
+    define('DB_USERNAME', 'djs93');
+    define('DB_PASSWORD', 'password');
+    define('DB_DATABASE', 'djs93');
+    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
